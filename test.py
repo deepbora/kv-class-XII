1bd249e7fcc9ef60d2123caaf8f9036e4f8cdbcc
Deep WindowsPC <deepbora435@gmail.com>
2024-04-06 21:42:50 +0530

    Kv next class

---
+++ test.py
@@ -0,0 +1,13 @@
+a=2
+print(a)
+# def IDontWantToThisFunctionTOCallAnotherFile(): 
+#     print(a)
+
+# IDontWantToThisFunctionTOCallAnotherFile()
+
+
+if __name__ =="__main__":
+    b=3
+    print(b)
+
+    print("deep")
---
+++ test1.py
@@ -0,0 +1,5 @@
+import test
+
+# print(test1.b)
+# test.IDontWantToThisFunctionTOCallAnotherFile()
+# test1.
